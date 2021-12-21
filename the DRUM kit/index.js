ee8163d8e7040843584b81f64b707b502dfc954b
mrfunky69 <87094553+mrfunky69@users.noreply.github.com>
2021-12-21 15:03:50 +0530

    Add files via upload

---
+++ the DRUM kit/index.js
@@ -0,0 +1,60 @@
+//document.querySelectorAll("button")[0].addEventListener("click",letsdo);
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", letsdo);
+}
+
+function letsdo() {
+  var buttoninner = this.innerHTML;
+
+ makesound(buttoninner);
+ animation(buttoninner);
+
+}
+addEventListener("keypress",function (event)
+{
+  makesound(event.key);
+  animation(event.key);
+});
+
+function makesound(key)
+{
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3")
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3")
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3")
+      tom3.play();
+      break;
+    case "d":
+      var snare = new Audio("sounds/snare.mp3")
+      snare.play();
+      break;
+    case "j":
+      var crash = new Audio("sounds/crash.mp3")
+      crash.play();
+      break;
+    case "k":
+      var kick = new Audio("sounds/kick-bass.mp3")
+      kick.play();
+      break;
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3")
+      tom4.play();
+      break;
+    default:console.log(buttoninner);
+
+  }
+
+}
+function animation(value)
+{
+    var r = document.querySelector("."+ value);
+    r.classList.add("pressed");
+    setTimeout(function(){ r.classList.remove("pressed")},200);
+}
